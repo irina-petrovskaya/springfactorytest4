a8bab113c3c6df0216f5646f165857df457bb772
irina <irina@me.com>
2018-02-20 12:02:05 +0300

    edit beans to return custom prefix

--- my-autoconfiguration-bundle4/src/main/java/com/mytests/springBoot/autoconfiguration/myautoconfigurationbundle4/configs/ConditionalOnPropertyWithInnerConfig1.java
+++ my-autoconfiguration-bundle4/src/main/java/com/mytests/springBoot/autoconfiguration/myautoconfigurationbundle4/configs/ConditionalOnPropertyWithInnerConfig1.java
@@ -10,18 +10,18 @@ import org.springframework.context.annotation.Configuration;
 @ConditionalOnProperty(name = "myprops.prop1", havingValue = "prop1")
 public class ConditionalOnPropertyWithInnerConfig1 {
 
-    @Bean(name = "ConditionalOnPropertyWithInnerConfig1_bean1")
+    @Bean(name = "mybean_ConditionalOnPropertyWithInnerConfig1_bean1")
     public Bean1 bean1() {
-        return new Bean1("ConditionalOnPropertyWithInnerConfig1_bean1");
+        return new Bean1("mybean_ConditionalOnPropertyWithInnerConfig1_bean1");
     }
 
     @Configuration
     @ConditionalOnProperty(name = "myprops.prop2", havingValue = "prop2")
     static class ConditionalOnPropertyWithInnerConfig1_Inner {
 
-        @Bean(name = "ConditionalOnPropertyWithInnerConfig1_inner_bean2")
+        @Bean(name = "mybean_ConditionalOnPropertyWithInnerConfig1_inner_bean2")
         public Bean2 bean2() {
-            return new Bean2("ConditionalOnPropertyWithInnerConfig1_inner_bean2");
+            return new Bean2("mybean_ConditionalOnPropertyWithInnerConfig1_inner_bean2");
         }
     }
 }
