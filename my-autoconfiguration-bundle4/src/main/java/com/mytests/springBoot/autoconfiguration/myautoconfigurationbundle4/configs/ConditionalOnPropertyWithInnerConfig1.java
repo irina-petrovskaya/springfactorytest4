4503df12313a2e368768296053030cb68524b49f
irina <irina@me.com>
2018-02-20 11:47:06 +0300

    add config

---
+++ my-autoconfiguration-bundle4/src/main/java/com/mytests/springBoot/autoconfiguration/myautoconfigurationbundle4/configs/ConditionalOnPropertyWithInnerConfig1.java
@@ -0,0 +1,27 @@
+package com.mytests.springBoot.autoconfiguration.myautoconfigurationbundle4.configs;
+
+import com.mytests.springBoot.autoconfiguration.myautoconfigurationbundle4.beans.Bean1;
+import com.mytests.springBoot.autoconfiguration.myautoconfigurationbundle4.beans.Bean2;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnProperty(name = "myprops.prop1")
+public class ConditionalOnPropertyWithInnerConfig1 {
+
+    @Bean(name = "ConditionalOnPropertyWithInnerConfig1_bean1")
+    public Bean1 bean1() {
+        return new Bean1("ConditionalOnPropertyWithInnerConfig1_bean1");
+    }
+
+    @Configuration
+    @ConditionalOnProperty("myprops.prop2")
+    static class ConditionalOnPropertyWithInnerConfig1_Inner {
+
+        @Bean(name = "ConditionalOnPropertyWithInnerConfig1_inner_bean2")
+        public Bean2 bean2() {
+            return new Bean2("ConditionalOnPropertyWithInnerConfig1_inner_bean2");
+        }
+    }
+}
