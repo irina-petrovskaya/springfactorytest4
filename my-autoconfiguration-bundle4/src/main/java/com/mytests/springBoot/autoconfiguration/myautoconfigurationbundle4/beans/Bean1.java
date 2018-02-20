757abc4f91077b359f1200c1fef3507a770af40b
irina <irina@me.com>
2018-02-20 11:44:41 +0300

    add 2 beans

---
+++ my-autoconfiguration-bundle4/src/main/java/com/mytests/springBoot/autoconfiguration/myautoconfigurationbundle4/beans/Bean1.java
@@ -0,0 +1,14 @@
+package com.mytests.springBoot.autoconfiguration.myautoconfigurationbundle4.beans;
+
+public class Bean1 {
+    private final String id;
+
+    public Bean1(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return id;
+    }
+}
