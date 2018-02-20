fd3605529498d5584f8ecf32da7477d61cd1a1ab
irina <irina@me.com>
2018-02-20 12:42:44 +0300

    test for conditional beans: first error to submit

---
+++ my-autoconfiguration-bundle4/src/main/java/com/mytests/springBoot/autoconfiguration/myautoconfigurationbundle4/configs/MainConfig.java
@@ -0,0 +1,9 @@
+package com.mytests.springBoot.autoconfiguration.myautoconfigurationbundle4.configs;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class MainConfig {
+}
