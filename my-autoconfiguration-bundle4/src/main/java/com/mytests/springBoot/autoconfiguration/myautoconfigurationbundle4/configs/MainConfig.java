12fd93a986daf10ce07101ee72c978023faad866
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2018-04-09 17:23:02 +0300

    no-related issue test (inner config beans referencing by names)

--- my-autoconfiguration-bundle4/src/main/java/com/mytests/springBoot/autoconfiguration/myautoconfigurationbundle4/configs/MainConfig.java
+++ my-autoconfiguration-bundle4/src/main/java/com/mytests/springBoot/autoconfiguration/myautoconfigurationbundle4/configs/MainConfig.java
@@ -1,9 +1,21 @@
 package com.mytests.springBoot.autoconfiguration.myautoconfigurationbundle4.configs;
 
+import com.mytests.springBoot.autoconfiguration.myautoconfigurationbundle4.beans.Bean1;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan
 public class MainConfig {
+
+    @Bean
+    public Bean1 bean1_from_main() {
+        return new Bean1("bean1_from_main");
+    }
+
+    @Configuration
+    public static class FooConfig {
+
+    }
 }
