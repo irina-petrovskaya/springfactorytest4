607b5046b8a9d2c731aaebaea4e062a557bdc0cf
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2018-10-22 16:13:08 +0300

    create test for wiring beans defined in the imported configs

--- use-my-autoconfiguration-bundle4/src/test/java/com/mytests/springboot/autoconfiguration/bundle4usage/usemyautoconfigurationbundle4/UseMyAutoconfigurationBundle4ApplicationTests.java
+++ use-my-autoconfiguration-bundle4/src/test/java/com/mytests/springboot/autoconfiguration/bundle4usage/usemyautoconfigurationbundle4/UseMyAutoconfigurationBundle4ApplicationTests.java
@@ -18,10 +18,24 @@ public class UseMyAutoconfigurationBundle4ApplicationTests {
     @Autowired
     private Environment environment;
 
+    @Autowired(required = false)
+    Bean1 bean1;
+
+    @Autowired(required = false)
+    Bean2 bean2;
+    @Autowired
+    private MyProperties myProperties;
+
 
     @Test
     public void test() {
-
+        if (myProperties.getProp1() != null) {
+            System.out.println("myprops.prop1 = "+myProperties.getProp1());
+        } else {
+            System.out.println("property myprops.prop1 is not set");
+        }
+        Assert.assertNotNull("property should be defined and have value 'prop1'",bean1);
+        Assert.assertNotNull("property should be defined and have value 'prop1'",bean2);
     }
 
 }
