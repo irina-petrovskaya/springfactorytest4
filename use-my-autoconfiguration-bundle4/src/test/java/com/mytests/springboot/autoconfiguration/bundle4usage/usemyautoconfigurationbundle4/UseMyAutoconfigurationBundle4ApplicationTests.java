ce62a57b41e2ab046ad6de626a768454ba25bfa9
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2019-07-12 15:34:02 +0300

    test method to check autowiring

--- use-my-autoconfiguration-bundle4/src/test/java/com/mytests/springboot/autoconfiguration/bundle4usage/usemyautoconfigurationbundle4/UseMyAutoconfigurationBundle4ApplicationTests.java
+++ use-my-autoconfiguration-bundle4/src/test/java/com/mytests/springboot/autoconfiguration/bundle4usage/usemyautoconfigurationbundle4/UseMyAutoconfigurationBundle4ApplicationTests.java
@@ -2,6 +2,8 @@ package com.mytests.springboot.autoconfiguration.bundle4usage.usemyautoconfigura
 
 import com.mytests.springBoot.autoconfiguration.myautoconfigurationbundle4.beans.Bean1;
 import com.mytests.springBoot.autoconfiguration.myautoconfigurationbundle4.beans.Bean2;
+import com.mytests.springBoot.autoconfiguration.myautoconfigurationbundle4.configs.components.Compo1;
+import com.mytests.springBoot.autoconfiguration.myautoconfigurationbundle4.configs.components.Compo2;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,10 +19,25 @@ public class UseMyAutoconfigurationBundle4ApplicationTests {
 
     @Autowired
     private Environment environment;
+    @Autowired
+    private Bean1 b1;
+    @Autowired
+    private Bean2 b2;
+
+
+    @Autowired
+    private Compo1 c1;
+    @Autowired
+    private Compo2 c2;
 
 
     @Test
     public void test() {
+        System.out.println("=================== test started ==================");
+        Assert.assertEquals("bean1_from_client", b1.toString());
+        Assert.assertEquals("bean2_autoconfigured", b2.toString());
+        Assert.assertEquals("compo2_autoconfigured", c2.getId());
+        Assert.assertEquals("compo1_from_client", c1.getId());
 
     }
 
